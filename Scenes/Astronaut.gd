fbcbf9b0f2976a0b8adea72e23a54eb3361c5cce
Hakim Rouatbi <hakim.rouatbi@pernod-ricard.com>
2020-10-04 01:48:14 +0200

    Change astronaut animation to AnimateSprite2D

--- Scenes/Astronaut.gd
+++ Scenes/Astronaut.gd
@@ -1,13 +1,8 @@
 extends KinematicBody2D
 
-onready var anim : AnimationPlayer = $AnimationPlayer
-
 var direction := Vector2()
 var speed := 20.0
 var collision_info
-
-func _ready():
-	anim.play("wiggle")
 
 func _process(delta):
 	collision_info = move_and_collide(direction * speed * delta)
