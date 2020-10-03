c42540b10a26255e12e2099a99ac34798a7f0a05
Hakim Rouatbi <hakim.rouatbi@pernod-ricard.com>
2020-10-04 01:28:39 +0200

    Change shuttle & astronauts to KinematicBody to handle collision detection

--- Scenes/Astronaut.gd
+++ Scenes/Astronaut.gd
@@ -1,16 +1,17 @@
-extends Area2D
+extends KinematicBody2D
 
 onready var anim : AnimationPlayer = $AnimationPlayer
 
 var direction := Vector2()
 var speed := 20.0
+var collision_info
 
 func _ready():
 	anim.play("wiggle")
 
 func _process(delta):
-	position += direction * speed * delta
+	collision_info = move_and_collide(direction * speed * delta)
+	if collision_info:
+		direction = direction.bounce(collision_info.normal)
 	rotate(2 * delta)
 
-func _on_Astronaut_area_entered(area):
-	print(area)
--- Scenes/Shuttle.gd
+++ Scenes/Shuttle.gd
@@ -1,15 +1,14 @@
-extends Area2D
+extends KinematicBody2D
 
-onready var pivot : Position2D = $Pivot
-onready var shuttle : Sprite =  $Pivot/Sprite
+onready var shuttle : Sprite = $Sprite
 const astronaut_scene : PackedScene = preload("res://Scenes/Astronaut.tscn")
 var rotation_speed : float = 3
 
 func _process(delta):
 	if Input.is_action_pressed("move_right"):
-		pivot.rotate(delta * rotation_speed)
+		rotate(delta * rotation_speed)
 	if Input.is_action_pressed("move_left"):
-		pivot.rotate(delta * rotation_speed * -1)
+		rotate(delta * rotation_speed * -1)
 
 func _input(event):
 	if event.is_action_pressed("launch_astronaut"):
@@ -18,5 +17,5 @@ func _input(event):
 func launch_astronaut():
 	var astro = astronaut_scene.instance()
 	astro.position = shuttle.global_position
-	astro.direction = (pivot.global_position - shuttle.global_position).normalized()
+	astro.direction = (global_position - shuttle.global_position).normalized()
 	get_tree().current_scene.add_child(astro)
